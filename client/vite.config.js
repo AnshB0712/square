d5aac107ad6c2580edac733d7f6c3d5bbed13ce3
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2024-05-16 13:51:28 +0530

    PWA try

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,33 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Square App",
+        short_name: "",
+        description: "",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
