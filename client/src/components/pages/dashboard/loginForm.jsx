11a48196e6610c950e4f80129875213b5aec571f
Anshul Borde <89180266+AnshB0712@users.noreply.github.com>
2024-05-23 11:58:26 +0530

    Update loginForm.jsx

--- client/src/components/pages/dashboard/loginForm.jsx
+++ client/src/components/pages/dashboard/loginForm.jsx
@@ -208,7 +208,7 @@ export function LoginForm() {
   const from = location.state?.from?.pathname || '/dashboard'
   const login = useLogin(`auth/login/${role}`)
 
-  const { setUser } = useAuthCtx()
+  const { user, setUser } = useAuthCtx()
 
   const handleLogin = async (details) => {
     login.mutate(
@@ -249,7 +249,7 @@ export function LoginForm() {
       }
     }
 
-    isSessionExists()
+    user.token && isSessionExists()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
